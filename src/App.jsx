5768e41750712659fbfb478cc9b8c2efe1a95c5d
MesudQuluzade <quluzademesud6@gmail.com>
2025-07-07 20:30:44 +0400

    counter

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Counter from './components/counter'
+
+function App() {
+  return (
+    <div className=" bg-red-50 min-h-screen px-8 py-10">
+
+      <Counter></Counter>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Counter.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+  const [counter, setCounter] = useState(20);
+
+ const inc = () => setCounter (counter +1)
+ const dec = () => (counter -1)
+ const res = () => setCounter(0)
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="flex items-center justify-center gap-4 mb-6">
+        <h1 className="text-3xl font-semibold">Counter App</h1>
+      </div>
+
+      <div className="text-5xl font-bold mb-10">{counter}</div>
+
+      <div className="flex gap-6">
+        <button
+          onClick={inc}
+          className="w-14 h-14 rounded-full bg-[#1C2735] text-white text-2xl hover:scale-105 transition"
+        >
+          +
+        </button>
+        <button
+          onClick={dec}
+          className="w-14 h-14 rounded-full bg-[#1C2735] text-white text-2xl hover:scale-105 transition"
+        >
+          -
+        </button>
+        <button
+          onClick={res}
+          className="w-14 h-14 rounded-full bg-[#1C2735] text-white text-2xl hover:scale-105 transition"
+        >
+          0
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
